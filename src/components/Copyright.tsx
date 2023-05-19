ceaf20b3a63780732b5a33d93d4b10b3e663d4ab
Antonio Carlos <antonie.dev@gmail.com>
2023-05-19 12:00:30 -0300

    add-components-and-login-oauth

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 por{" "}
+      <a
+        href="https://www.linkedin.com/in/antonio-carlos-44b106129/"
+        className="underline hover:text-gray-100"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Antonio Carlos{" "}
+      </a>
+    </div>
+  );
+}
